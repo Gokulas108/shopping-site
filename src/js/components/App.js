673c9eb25769ef2023f02c2ef481e0d07ab18036
Gokulas108 <61143791+Gokulas108@users.noreply.github.com>
2020-04-28 13:32:33 +0530

    Shopping Site DEMO

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -14,7 +14,7 @@ const App = () => (
 
         <Router>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Cognitiiiica AI</a>
+                <a className="navbar-brand" href="#">SHOP DEMO</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
